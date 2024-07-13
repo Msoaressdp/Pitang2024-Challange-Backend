36b2fa59cdcfe9f2d98ad1fb56b8e44fd0350af6
Msoaressdp <mssdp@cesar.school>
2024-07-13 13:08:29 -0300

    feat: add store method to controller

--- src/controllers/appointment.controller.mjs
+++ src/controllers/appointment.controller.mjs
@@ -13,7 +13,28 @@ export default class AppointmentController {
     destroy(request, response) {}
     getOne(request, response) {}
     index(request,response) {}
-    store(request, response) {}
+    store(request, response) {
+      const appointment = request.body;
+  
+      const { sucess, data, error } = appointmentSchema.safeParse({
+  
+        name: appointment.name,
+        birthDate: appointment.birthDate,
+        scheduledDate: appointment.scheduledDate,
+  
+      });
+  
+      if (!sucess){
+        return response.status(400).send(error);
+      }
+  
+      appointments.push(data);
+  
+      response.send({ message: 'store', data });
+  
+  
+    }
+    
     update(request, response) {}
   
   }
