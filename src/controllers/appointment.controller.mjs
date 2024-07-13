d139fa66db4b59707b3b225888c4d925a2d80d8e
Msoaressdp <mssdp@cesar.school>
2024-07-13 12:33:00 -0300

    feat: add appointment controller

---
+++ src/controllers/appointment.controller.mjs
@@ -0,0 +1,8 @@
+export default class AppointmentController {
+    destroy(request, response) {}
+    getOne(request, response) {}
+    index(request,response) {}
+    store(request, response) {}
+    update(request, response) {}
+  
+  }
