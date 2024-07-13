750d2fd7a7d9e40fcb041eae89b0cbf829c61f47
Msoaressdp <mssdp@cesar.school>
2024-07-13 13:01:09 -0300

    feat: add zod schema to controller

--- src/controllers/appointment.controller.mjs
+++ src/controllers/appointment.controller.mjs
@@ -1,4 +1,15 @@
+import z from 'zod';
+
+const appointmentSchema = z.object({
+  name: z.string(),
+  birthDate: z.date(),
+  scheduledDate: z.date(),
+});
+
+let appointments = [];
+
 export default class AppointmentController {
+
     destroy(request, response) {}
     getOne(request, response) {}
     index(request,response) {}
