ec71d06a843d658085d826836c4630fa043412c3
Msoaressdp <mssdp@cesar.school>
2024-07-13 16:18:00 -0300

    chore: setup cors middleware

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 import appointmentRoutes from './routes/appointment.router.mjs';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = express();
 
+server.use(cors()); 
 server.use(express.json());
 server.use(appointmentRoutes);
 
